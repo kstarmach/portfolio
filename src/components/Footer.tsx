53944efdca304b54a400a0030282c2a2e9d98420
kstarmach <kstarmach@outlook.com>
2024-02-18 15:20:29 +0100

    fix: Footer always current year

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer className="bg-transparent text-center py-2 mt-auto">
-            <p>&copy; 2023 <span className="font-bold">Kamil Starmach</span></p>
+            <p>&copy;{(new Date().getFullYear())} <span className="font-bold">Kamil Starmach</span></p>
         </footer>
     );
 };
