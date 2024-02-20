f3c96de6ccb487e9ed0080d9a3a02e98ba930cf2
kstarmach <kstarmach@outlook.com>
2024-02-20 17:30:19 +0100

    fix: Upload correct profile pic

--- src/components/Redirect.tsx
+++ src/components/Redirect.tsx
@@ -5,8 +5,9 @@ const Redirect = () => {
     return (
 
         <div className="flex justify-center sm:gap-14 gap-8 my-4 sm:my-8">
-            <img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg" className="rounded-full  h-44 w-44   object-cover border-solid border-2 " />
-            <div className="grid gap-1">
+            <img src="./profile-pic.png" className="rounded-full h-44 w-44 object-cover border-solid border-2 border-gray-600 dark:border-gray-400" />
+
+            <div className="grid gap-2">
                 <a href="https://github.com/kstarmach" target="_blank" rel="noopener noreferrer" className="border border-gray-600 dark:border-white rounded-full px-3 py-2 flex items-center  hover:text-gray-400 hover:border-gray-400 dark:hover:text-gray-400 dark:hover:border-gray-400">
                     <FaGithub className="w-5 h-5 mr-2" />
                     <span className="hidden sm:inline">GitHub </span>
