5f380642f98f3138916ffbfebd47b68d1aba6e4f
kstarmach <kstarmach@outlook.com>
2024-02-21 15:01:48 +0100

    fix: New link to old project

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -30,7 +30,7 @@ const projectsData: Project[] = [
         content: 'Everyday Checklist is a web application built with *ReactJS* and *TypeScript* that allows users to create and manage daily to-do lists.\n\n The frontend has a modern UI design and leverages the power of *ReactJS* to provide a smooth user experience. Users can create accounts and log in to access their personalization lists.\n\n The application is built on a *TypeScript* and *ExpressJS* backend, and uses *Mongoose* to connect to a *MongoDB database*. It provides a reliable and scalable infrastructure to securely store user data.\n\n The main purpose of *Every Day Checklist* is to create a simple, yet effective, tool for users to track their daily tasks and build positive habits. Users can customize their to-do lists based on their daily routines and goals, and the application refreshes each day to provide a clean slate for new tasks. The platform also provides *statistics* to help users track their progress and stay motivated.\n\n Overall, *Every Day Checklist* is a powerful productivity tool that leverages the latest web technologies to help users build better habits and achieve their goals.',
         shortContent: 'Every Day Checklist is a ReactJS and TypeScript web application that allows users to create and manage daily to-do lists.',
         createDate: new Date("2023-03-29"),
-        demoUrl: 'https://example.com/demo1',
+        demoUrl: 'https://every-day-checklist.vercel.app/',
         githubUrl: 'https://github.com/kstarmach/every-day-checklist',
         imgUrl: '/everyday-todo-screenshot.png',
     }
