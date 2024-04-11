b893bd71c7105e5e75b7fae87764398aa95a897e
Kamil Starmach <kstarmach@outlook.com>
2024-04-11 11:31:45 +0200

    fix: Project list style error

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -12,44 +12,48 @@ function ProjectListItem({ project }: { project: Project }) {
 
     return (
         <>
-            <Link href={`/projects/${project.id}`} passHref>
-                <div
-                    className="flex flex-col gap-2"
-                    onMouseEnter={() => setIsHovered(true)}
-                    onMouseLeave={() => setIsHovered(false)}
-                >
+            <Link
+                href={`/projects/${project.id}`}
+                passHref
+                className=" rounded-lg  cursor-pointer flex flex-col gap-2"
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+            >
 
-                    <div>
-                        <h3 className="text-xl font-extrabold my-2" tabIndex={0}>
-                            {project.title}
-                        </h3>
-                        {project.technology.map((t, idx) =>
-                            <TechnologyIcon technology={t} key={idx} />
-                        )}
-                        <p className="text-gray-500 text-sm mt-4" aria-label="Date">
-                            {formattedDate}
-                        </p>
-                    </div>
-                    <p className="my-4 text-lg sm:text-xl">
-                        {project.shortContent}
-                    </p>
-                    <span className={`border-b-2 border-gray-900 hover:border-b-2 hover:border-gray-400 hover:text-gray-400`}>
-                        <b>Read more &rarr;</b>
-                    </span>
-
-                    <Image
-                        src={project.imgUrl}
-                        priority={true}
-                        alt="Example image from Picsum"
-                        width={600}
-                        height={384}
-                        className={`rounded-xl w-full h-auto transition-all duration-500 ${isHovered ? 'transform scale-105' : ''}`}
-                    />
-
-
+                <h3 className="text-xl font-extrabold " tabIndex={0}>
+                    {project.title}
+                </h3>
+                <div className="flex ">
+                    {project.technology.map((t, idx) =>
+                        <TechnologyIcon technology={t} key={idx} />
+                    )}
                 </div>
+
+                <p className="text-gray-500 text-sm " aria-label="Date">
+                    {formattedDate}
+                </p>
+
+                <p className=" text-lg sm:text-xl">
+                    {project.shortContent}
+                </p>
+
+                <span className={`mb-4 hover:text-gray-400`}>
+                    <b>Read more &rarr;</b>
+                </span>
+
+                <Image
+                    src={project.imgUrl}
+                    priority={true}
+                    alt="Example image from Picsum"
+                    width={600}
+                    height={384}
+                    className={`rounded-xl w-full h-auto transition-all duration-500 ${isHovered ? 'transform scale-105' : ''}`}
+                />
+
+
+
             </Link>
-            <hr className="border-gray-300 my-8" />
+
         </>
     );
 }
@@ -81,10 +85,13 @@ export default function Projects() {
 
     return (
         <>
-
-            {projectList.map(p => (
-                <ProjectListItem project={p} key={p.id} />
+            {projectList.map((p, index) => (
+                <div key={p.id}>
+                    <ProjectListItem project={p} />
+                    {index !== projectList.length - 1 && <hr className="border-gray-300 my-8" />}
+                </div>
             ))}
         </>
     );
+
 }
